d8a6dc8050eb139c8c3be46b08f692426dcb7629
Maksim Reshetov <96242213+mreshetov@users.noreply.github.com>
2023-10-16 13:50:35 +0300

    Update build-fineract.sh

--- fineract-ext/build-fineract.sh
+++ fineract-ext/build-fineract.sh
@@ -8,9 +8,3 @@ git reset --hard 7741340ef302e0e651e46cd6f4d80bc010f54e4b
 rm -rf ./custom/acme
 mkdir ./custom/finto
 cp -rf ../custom-module/* ./custom/finto/
-
-./gradlew :custom:docker:jibDockerBuild -D'jib.to.image'=nexus.finlab.dev/fineract-ext:${FINERACT_EXT_VERSION}
-cd ..
-rm -rf fineract
-
-docker image push nexus.finlab.dev/fineract-ext:${FINERACT_EXT_VERSION}
