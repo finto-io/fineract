a0b12216cb35de53c3e900283e8a7942cdd42294
Dmitry-Pinchuk-leadex <jenkins@jenkins.finto.io>
2024-01-09 14:09:28 +0000

    feature/FIN2-22773 - FIN2-22773 delete push image row in build script

--- fineract-ext/build-fineract.sh
+++ fineract-ext/build-fineract.sh
@@ -8,5 +8,3 @@ git reset --hard 7741340ef302e0e651e46cd6f4d80bc010f54e4b
 rm -rf ./custom/acme
 mkdir ./custom/finto
 cp -rf ../custom-module/* ./custom/finto/
-
-docker image push nexus.finlab.dev/fineract-ext:${FINERACT_EXT_VERSION}
